7433afc60b58c3e104550f9b1b6dc09e7f5cb341
Juyong <ht9943@naver.com>
2021-12-03 00:56:41 +0900

    1203

---
+++ app.js
@@ -0,0 +1,23 @@
+const a = 5;  // 보통 const 를 쓰고 
+const b = 2;
+let myName = "juyong";  // 변경하고 싶은게 있으면 let을 사용한다.   var은 그냥 쓰지 말자
+
+
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
+console.log("hello" + myName)
+
+myName = "leejuyong"
+
+console.log("your ne name is " + myName)
+
+
+const amIFat = true;
+console.log(amIFat)
+
+const amITall = null;    // 값이 없다.
+console.log(amIFat)
+
+const amIBig;       // undefined  정의되지 않았다.
+console.log(amIBig)
